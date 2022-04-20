27bda9cd25645ef27a1be6d650b1664a8f3f544a
Sébastien Veilleux <sebastien.veilleux@bba.ca>
2022-04-20 10:20:04 -0400

    Add file for run

---
+++ run/cleanup.bat
@@ -0,0 +1,8 @@
+cd Main
+git update-ref -d refs/remotes/tfs/default
+git config --remove-section git-tfs
+git config --remove-section tfs
+git config --remove-section tfs-remote.default
+rd .git\refs\remotes /S/Q
+rd .git\logs\refs\remotes /S/Q
+cd ..
