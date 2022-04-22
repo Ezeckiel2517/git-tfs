a29a221f5517d595f8d87ebc6f2cb50885f7d3a3
Sébastien Veilleux <sebastien.veilleux@bba.ca>
2022-04-22 14:08:41 -0400

    Add options to disable renames detection

--- run/cleanup.bat
+++ run/cleanup.bat
@@ -3,6 +3,7 @@ git update-ref -d refs/remotes/tfs/default
 git config --remove-section git-tfs
 git config --remove-section tfs
 git config --remove-section tfs-remote.default
+REM git config --add diff.renames false
 rd .git\refs\remotes /S/Q
 rd .git\logs\refs\remotes /S/Q
 cd ..
